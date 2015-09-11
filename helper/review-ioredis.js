bc1c4edfd838529e5f8dcc8584c6afa3bb12c0ef
woodtalk <woodtalk@daum.net>
2015-09-11 19:39:07 +0900

    기존 코드 버그 수정

--- helper/review-ioredis.js
+++ helper/review-ioredis.js
@@ -8,72 +8,13 @@ const FakeIoRedis = require('../index');
 function* review() {
     const client = new (this.creator)();
 
-    yield client.del('myzset');
-
-    console.log(yield client.zadd('myzset', 1, 'one'));
-    console.log(yield client.zadd('myzset', 1, 'one'));
-    console.log(yield client.zadd('myzset', 2, 'one'));
-
-    console.log(yield client.zrange('myzset', 0, -1));
-    console.log(yield client.zrange('myzset', 0, -1, 'withscores'));
-
-    console.log(yield client.zadd('myzset', 3, 'three'));
-    console.log(yield client.zadd('myzset', 4, 'four'));
-
-    console.log(yield client.zrange('myzset', 0, -1));
-    console.log(yield client.zrange('myzset', 0, -1, 'withscores'));
-
-    console.log(yield client.zrem('myzset', 'one'));
-
-    console.log(yield client.zrange('myzset', 0, -1));
-    console.log(yield client.zrange('myzset', 0, -1, 'withscores'));
-
-    console.log(yield client.zrank('myzset', 'three'));
-    console.log(yield client.zrank('myzset', 'four'));
-    console.log(yield client.zrank('myzset', 'one'));
-
-    console.log(yield client.zadd('myzset', 1, 'one'));
-    console.log(yield client.zadd('myzset', 2, 'two'));
-
-    console.log(yield client.zrange('myzset', 0, -1));
-    console.log(yield client.zrangebyscore('myzset', '-inf', '+inf'));
-    console.log(yield client.zrangebyscore('myzset', 2, '+inf'));
-    console.log(yield client.zrangebyscore('myzset', 2, 3));
-    console.log(yield client.zrangebyscore('myzset', '(2', 3));
-    console.log(yield client.zrangebyscore('myzset', '(2', '3'));
-    console.log(yield client.zrangebyscore('myzset', '(2', '(3'));
-    console.log(yield client.zrangebyscore('myzset', '(2', 3, 'withscores'));
-
-    console.log(yield client.zadd('myzset', 1, 'one1', 1, 'one2', 1, 'one2'));
-    console.log(yield client.zrange('myzset', 0, -1));
-    console.log(yield client.zrange('myzset', 0, -1, 'withscores'));
-
-    console.log(yield client.zrange('myzset', 2, 2));
-    console.log(yield client.zrange('myzset', 2, 3));
-    console.log(yield client.zrange('myzset', 1, 3));
-    console.log(yield client.zrange('myzset', 0, 3));
-    console.log(yield client.zrange('myzset', 0, 0));
-    console.log(yield client.zrange('myzset', 0, 0, 'withscores'));
-
-    console.log(yield client.zrange('myzset', 0, -1));
-
-    console.log(yield client.zremrangebyscore('myzset', 1, 1));
-
-    console.log(yield client.zrange('myzset', 0, -1));
-
-    console.log(yield client.zadd('myzset', 1, 'one', 1, 'one1', 1, 'one2'));
-    console.log(yield client.zrange('myzset', 0, -1));
-
-    console.log(yield client.zremrangebyscore('myzset', 1, 2));
-
-    console.log(yield client.zrange('myzset', 0, -1));
-
-    console.log(yield client.zadd('myzset', 1, 'one', 1, 'one1', 1, 'one2', 2, 'two'));
-    console.log(yield client.zrange('myzset', 0, -1));
-
-    console.log(yield client.zremrangebyrank('myzset', 1, 2));
-
-    console.log(yield client.zrange('myzset', 0, -1));
+    yield client.set('string', 'a');
+    try {
+        yield client.srem('string', 's', 'y');
+    } catch (e) {
+        console.log(e.name);
+        console.error(e.stack);
+    }
 
     client.disconnect();
 }
